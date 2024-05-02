8c6f84c8ceb0214f5dd0dce5ec51d21804de688e
Maryna Zubkova <maryna.zubkova@lanars.com>
2024-05-02 21:44:58 +0300

    Install astro-i18next; add i18n helpers; translations, select

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    astroI18next(),
   ],
 });
