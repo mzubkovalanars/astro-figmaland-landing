df519d08c27e4c579ed024ce31cc392c154a55a1
Maryna Zubkova <maryna.zubkova@lanars.com>
2024-05-03 19:51:28 +0300

    Update translations, useTranslations

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -13,8 +13,8 @@ export function getLangFromUrl(url: URL) {
 }
 
 export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-    return ui[lang][key] || ui[defaultLang][key];
+  return function t(key: string) {
+    return ui[lang][key as keyof (typeof ui)["en"]] || key;
   };
 }
 
