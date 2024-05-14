cc61a7bbb0d5e687b9d2e3bfe9f1b3d793bd2ddc
Maryna Zubkova <maryna.zubkova@lanars.com>
2024-05-14 12:57:36 +0300

    Add icons, translations, constants, accordion for footer

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,50 @@
+import SocialIcon1 from "@public/twitter-outlined.svg";
+import SocialIcon2 from "@public/facebook-filled.svg";
+import SocialIcon3 from "@public/linkedin-filled.svg";
+
 export const navLinks = [
   { href: "/", title: "nav.home" },
   { href: "#product", title: "nav.product" },
   { href: "#about", title: "nav.about" },
   { href: "/blog", title: "nav.blog" },
   { href: "#contact", title: "nav.contact" },
+];
+
+export const navLinksFooter = [
+  {
+    title: "fingertipe",
+    links: [
+      { href: "/", title: "nav.home" },
+      { href: "/examples", title: "examples" },
+      { href: "/pricing", title: "pricing" },
+      { href: "/updates", title: "updates" },
+    ],
+  },
+  {
+    title: "resources",
+    links: [
+      { href: "/", title: "nav.home" },
+      { href: "/examples", title: "examples" },
+      { href: "/pricing", title: "pricing" },
+      { href: "/updates", title: "updates" },
+    ],
+  },
+  {
+    title: "about",
+    links: [
+      { href: "/", title: "nav.home" },
+      { href: "/examples", title: "examples" },
+      { href: "/pricing", title: "pricing" },
+      { href: "/updates", title: "updates" },
+    ],
+  },
+];
+
+export const ADDRESS_COMPANY = "7480 Mockingbird Hill";
+export const PHONE_COMPANY = "(239) 555-0108";
+
+export const socialContactLinks = [
+  { href: "#", alt: "twitter figmaland", img: SocialIcon1 },
+  { href: "#", alt: "facebook figmaland", img: SocialIcon2 },
+  { href: "#", alt: "linkedin figmaland", img: SocialIcon3 },
 ];
