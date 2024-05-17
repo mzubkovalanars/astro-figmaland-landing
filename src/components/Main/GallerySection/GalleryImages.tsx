b35757063ce55179236b837b3c5faa97ec4288af
Maryna Zubkova <maryna.zubkova@lanars.com>
2024-05-17 21:04:42 +0300

    Create all sections for Main

---
+++ src/components/Main/GallerySection/GalleryImages.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useTranslations } from "@/i18n/utils";
+import { useMediaQuery } from "@uidotdev/usehooks";
+import {
+  galleryImages,
+  galleryImagesMob,
+  DISPLAY_IMAGES,
+  DISPLAY_IMAGES_MOB,
+} from "@/utils/constants";
+
+interface GalleryImagesProps {
+  lang: "en" | "no";
+}
+
+const GalleryImages: React.FC<GalleryImagesProps> = ({ lang }) => {
+  const [showAll, setShowAll] = useState<boolean>(false);
+  const t = useTranslations(lang);
+  const isMobileDevice = useMediaQuery("only screen and (max-width: 560px)");
+
+  const imagesToDisplay = isMobileDevice ? galleryImagesMob : galleryImages;
+
+  const countDisplayImages = isMobileDevice
+    ? DISPLAY_IMAGES_MOB
+    : DISPLAY_IMAGES;
+
+  const displayedImages = showAll
+    ? imagesToDisplay
+    : imagesToDisplay.slice(0, countDisplayImages);
+
+  return (
+    <>
+      <div className='flex flex-wrap items-center justify-center gap-[33px] max-w-[1097px] my-11'>
+        {displayedImages.map((image, i) => (
+          <img
+            key={image.src + i}
+            src={image.src}
+            alt={image.alt}
+            className='h-auto max-w-full rounded-[12px]'
+          />
+        ))}
+      </div>
+      {showAll ? null : (
+        <button
+          onClick={() => setShowAll(!showAll)}
+          className='px-4 py-2 btn btn-secondary small'
+        >
+          {t("btn-more")}
+        </button>
+      )}
+    </>
+  );
+};
+
+export default GalleryImages;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,3 @@
-import SocialIcon1 from "@public/twitter-outlined.svg";
-import SocialIcon2 from "@public/facebook-filled.svg";
-import SocialIcon3 from "@public/linkedin-filled.svg";
-
 export const navLinks = [
   { href: "/", title: "nav.home" },
   { href: "#product", title: "nav.product" },
@@ -44,7 +40,110 @@ export const ADDRESS_COMPANY = "7480 Mockingbird Hill";
 export const PHONE_COMPANY = "(239) 555-0108";
 
 export const socialContactLinks = [
-  { href: "#", alt: "twitter figmaland", img: SocialIcon1 },
-  { href: "#", alt: "facebook figmaland", img: SocialIcon2 },
-  { href: "#", alt: "linkedin figmaland", img: SocialIcon3 },
+  { href: "#", alt: "twitter figmaland", src: "/icons/twitter-outlined.svg" },
+  { href: "#", alt: "facebook figmaland", src: "/icons/facebook-filled.svg" },
+  { href: "#", alt: "linkedin figmaland", src: "/icons/linkedin-filled.svg" },
 ];
+
+export const featureCards = [
+  {
+    title: "main.features.title1",
+    description: "main.features.desc1",
+    src: "/icons/round-restaurant-menu.svg",
+  },
+  {
+    title: "main.features.title2",
+    description: "main.features.desc2",
+    src: "/icons/alien-outline.svg",
+  },
+  {
+    title: "main.features.title3",
+    description: "main.features.desc3",
+    src: "/icons/all-inclusive.svg",
+  },
+];
+
+export const contentCards = [
+  {
+    title: "main.contents.title1",
+    description: "main.contents.desc1",
+    src: "/images/macbook-pro.png",
+    imgWidth: "405",
+    imgHeight: "226",
+    btnText: "btn-sign-up",
+  },
+  {
+    title: "main.contents.title2",
+    description: "main.contents.desc2",
+    src: "/images/boards-notifications.png",
+    imgWidth: "379",
+    imgHeight: "276",
+    btnText: "btn-try-for-free",
+  },
+];
+
+export const DISPLAY_IMAGES = 7;
+export const DISPLAY_IMAGES_MOB = 4;
+
+export const galleryImages = [
+  { src: "/images/gallery1.png", alt: "Image 1" },
+  { src: "/images/gallery2.png", alt: "Image 2" },
+  { src: "/images/gallery3.png", alt: "Image 3" },
+  { src: "/images/gallery4.png", alt: "Image 4" },
+  { src: "/images/gallery5.png", alt: "Image 5" },
+  { src: "/images/gallery6.png", alt: "Image 6" },
+  { src: "/images/gallery7.png", alt: "Image 7" },
+  { src: "/images/gallery1.png", alt: "Image 1" },
+  { src: "/images/gallery2.png", alt: "Image 2" },
+  { src: "/images/gallery3.png", alt: "Image 3" },
+];
+
+export const galleryImagesMob = [
+  { src: "/images/gallery1-mob.png", alt: "Image 1" },
+  { src: "/images/gallery2-mob.png", alt: "Image 2" },
+  { src: "/images/gallery3-mob.png", alt: "Image 3" },
+  { src: "/images/gallery4-mob.png", alt: "Image 4" },
+  { src: "/images/gallery1-mob.png", alt: "Image 1" },
+  { src: "/images/gallery2-mob.png", alt: "Image 2" },
+  { src: "/images/gallery3-mob.png", alt: "Image 3" },
+];
+
+export const partnersImages = [
+  { src: "/icons/logos_apple-app-store.svg", alt: "Image 1" },
+  { src: "/icons/logos_apiary.svg", alt: "Image 2" },
+  { src: "/icons/logos_android-icon.svg", alt: "Image 3" },
+  { src: "/icons/logos_basecamp.svg", alt: "Image 4" },
+  { src: "/icons/logos_airbnb.svg", alt: "Image 4" },
+  { src: "/icons/logos_ibm.svg", alt: "Image 4" },
+  { src: "/icons/logos_apple-app-store.svg", alt: "Image 1" },
+  { src: "/icons/logos_apiary.svg", alt: "Image 2" },
+];
+
+export const testimonialCards = [
+  {
+    title: "main.testimonials.title1",
+    role: "main.testimonials.role1",
+    description: "main.testimonials.desc1",
+    src: "/images/avatar1.png",
+  },
+  {
+    title: "main.testimonials.title2",
+    role: "main.testimonials.role2",
+    description: "main.testimonials.desc2",
+    src: "/images/avatar2.png",
+  },
+  {
+    title: "main.testimonials.title3",
+    role: "main.testimonials.role3",
+    description: "main.testimonials.desc3",
+    src: "/images/avatar3.png",
+  },
+  {
+    title: "main.testimonials.title4",
+    role: "main.testimonials.role4",
+    description: "main.testimonials.desc4",
+    src: "/images/avatar4.png",
+  },
+];
+
+export const COUNT_ITEM_IN_COLUMN = 2;
