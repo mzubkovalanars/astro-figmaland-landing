5d596adb0c000ab6b8348464355b495b446f7e66
Maryna Zubkova <maryna.zubkova@lanars.com>
2024-05-22 11:34:13 +0300

    Update Header styles, const links

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
 export const navLinks = [
   { href: "/", title: "nav.home" },
-  { href: "#product", title: "nav.product" },
-  { href: "#about", title: "nav.about" },
+  { href: "product", title: "nav.product" },
+  { href: "about", title: "nav.about" },
   { href: "/blog", title: "nav.blog" },
-  { href: "#contact", title: "nav.contact" },
+  { href: "contact", title: "nav.contact" },
 ];
 
 export const navLinksFooter = [
