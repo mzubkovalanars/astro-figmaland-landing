cc808216f52b25da77a182b3850a3eba96eca4b5
Maryna Zubkova <maryna.zubkova@lanars.com>
2024-05-27 18:34:38 +0300

    Add ssr @astrojs/vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
-
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
     applyBaseStyles: false
-  }), astroI18next(), mdx()]
+  }), astroI18next(), mdx()],
+  output: "server",
+  adapter: vercel()
 });
