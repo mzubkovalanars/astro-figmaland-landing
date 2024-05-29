cc3a90d268d043b17b86e21298237bbba41ed2ec
Maryna Zubkova <maryna.zubkova@lanars.com>
2024-05-29 13:33:12 +0300

    Add Image component from Astro; add width, height, srcset props

--- src/components/Main/PartnersSection/PartnersLogo.tsx
+++ src/components/Main/PartnersSection/PartnersLogo.tsx
@@ -23,6 +23,8 @@ const PartnersLogo: React.FC<PartnersLogoProps> = ({ lang }) => {
             <img
               src={image.src}
               alt={image.alt}
+              width={image.width}
+              height={image.height}
               className='h-auto max-h-[60px] sm:max-h-[50px] max-w-full rounded-[12px]'
             />
           </div>
