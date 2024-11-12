a9f0fa2185f9cb14cbf20ccc6e9000660cdbbe24
Maryna <maryna.zubkova@lanars.com>
2024-11-12 20:17:44 +0200

    Update imports images in consts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,45 @@
+import SocialIcon1 from '@assets/icons/twitter-outlined.svg';
+import SocialIcon2 from '@assets/icons/facebook-filled.svg';
+import SocialIcon3 from '@assets/icons/linkedin-filled.svg';
+import FeatureIcon1 from '@assets/icons/round-restaurant-menu.svg?url';
+import FeatureIcon2 from '@assets/icons/alien-outline.svg?url';
+import FeatureIcon3 from '@assets/icons/all-inclusive.svg?url';
+import ContentImg1 from '@assets/images/macbook-pro.png?url?url';
+import ContentImg2 from '@assets/images/boards-notifications.png?url';
+import GalleryImg1 from '@assets/images/gallery1.webp?url';
+import GalleryImg1X2 from '@assets/images/gallery1-x2.webp?url';
+import GalleryImg2 from '@assets/images/gallery2.webp?url';
+import GalleryImg2X2 from '@assets/images/gallery2-x2.webp?url';
+import GalleryImg3 from '@assets/images/gallery3.webp?url';
+import GalleryImg3X2 from '@assets/images/gallery3-x2.webp?url';
+import GalleryImg4 from '@assets/images/gallery4.webp?url';
+import GalleryImg4X2 from '@assets/images/gallery4-x2.webp?url';
+import GalleryImg5 from '@assets/images/gallery5.webp?url';
+import GalleryImg5X2 from '@assets/images/gallery5-x2.webp?url';
+import GalleryImg6 from '@assets/images/gallery6.webp?url';
+import GalleryImg6X2 from '@assets/images/gallery6-x2.webp?url';
+import GalleryImg7 from '@assets/images/gallery7.webp?url';
+import GalleryImg7X2 from '@assets/images/gallery7-x2.webp?url';
+import GalleryImgMob1 from '@assets/images/gallery1-mob.webp?url';
+import GalleryImgMob1X2 from '@assets/images/gallery1-mob-x2.webp?url';
+import GalleryImgMob2 from '@assets/images/gallery2-mob.webp?url';
+import GalleryImgMob2X2 from '@assets/images/gallery2-mob-x2.webp?url';
+import GalleryImgMob3 from '@assets/images/gallery3-mob.webp?url';
+import GalleryImgMob3X2 from '@assets/images/gallery3-mob-x2.webp?url';
+import GalleryImgMob4 from '@assets/images/gallery4-mob.webp?url';
+import GalleryImgMob4X2 from '@assets/images/gallery4-mob-x2.webp?url';
+import PartnersIcon1 from '@assets/icons/logos_apple-app-store.svg?url';
+import PartnersIcon2 from '@assets/icons/logos_apiary.svg?url';
+import PartnersIcon3 from '@assets/icons/logos_android-icon.svg?url';
+import PartnersIcon4 from '@assets/icons/logos_basecamp.svg?url';
+import PartnersIcon5 from '@assets/icons/logos_airbnb.svg?url';
+import PartnersIcon6 from '@assets/icons/logos_ibm.svg?url';
+import PartnersIcon7 from '@assets/icons/logos_apple-app-store.svg?url';
+import TestimonialImg1 from '@assets/images/avatar1.png?url';
+import TestimonialImg2 from '@assets/images/avatar2.png?url';
+import TestimonialImg3 from '@assets/images/avatar3.png?url';
+import TestimonialImg4 from '@assets/images/avatar4.png?url';
+
 export const navLinks = [
   { href: '/', title: 'nav.home' },
   { href: '/products', title: 'nav.products' },
@@ -40,26 +82,26 @@ export const ADDRESS_COMPANY = '7480 Mockingbird Hill';
 export const PHONE_COMPANY = '(239) 555-0108';
 
 export const socialContactLinks = [
-  { href: '#', alt: 'twitter figmaland', src: '/src/assets/icons/twitter-outlined.svg' },
-  { href: '#', alt: 'facebook figmaland', src: '/src/assets/icons/facebook-filled.svg' },
-  { href: '#', alt: 'linkedin figmaland', src: '/src/assets/icons/linkedin-filled.svg' },
+  { href: '#', alt: 'twitter figmaland', src: SocialIcon1 },
+  { href: '#', alt: 'facebook figmaland', src: SocialIcon2 },
+  { href: '#', alt: 'linkedin figmaland', src: SocialIcon3 },
 ];
 
 export const featureCards = [
   {
     title: 'main.features.title1',
     description: 'main.features.desc1',
-    src: '/src/assets/icons/round-restaurant-menu.svg',
+    src: FeatureIcon1,
   },
   {
     title: 'main.features.title2',
     description: 'main.features.desc2',
-    src: '/src/assets/icons/alien-outline.svg',
+    src: FeatureIcon2,
   },
   {
     title: 'main.features.title3',
     description: 'main.features.desc3',
-    src: '/src/assets/icons/all-inclusive.svg',
+    src: FeatureIcon3,
   },
 ];
 
@@ -67,7 +109,7 @@ export const contentCards = [
   {
     title: 'main.contents.title1',
     description: 'main.contents.desc1',
-    src: '/src/assets/images/macbook-pro.png',
+    src: ContentImg1,
     imgWidth: 405,
     imgHeight: 226,
     btnText: 'btn-sign-up',
@@ -75,7 +117,7 @@ export const contentCards = [
   {
     title: 'main.contents.title2',
     description: 'main.contents.desc2',
-    src: '/src/assets/images/boards-notifications.png',
+    src: ContentImg2,
     imgWidth: 379,
     imgHeight: 276,
     btnText: 'btn-try-for-free',
@@ -87,71 +129,71 @@ export const DISPLAY_IMAGES_MOB = 4;
 
 export const galleryImages = [
   {
-    src: '/src/assets/images/gallery1.webp',
-    src2x: '/src/assets/src/assets/images/gallery1-x2.webp',
+    src: GalleryImg1,
+    src2x: GalleryImg1X2,
     alt: 'Image 1',
     width: '225',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery2.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImg2,
+    src2x: GalleryImg2X2,
     alt: 'Image 2',
     width: '225',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery3.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImg3,
+    src2x: GalleryImg3X2,
     alt: 'Image 3',
     width: '225',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery4.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImg4,
+    src2x: GalleryImg4X2,
     alt: 'Image 4',
     width: '225',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery5.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImg5,
+    src2x: GalleryImg5X2,
     alt: 'Image 5',
     width: '395',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery6.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImg6,
+    src2x: GalleryImg6X2,
     alt: 'Image 6',
     width: '225',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery7.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImg7,
+    src2x: GalleryImg7X2,
     alt: 'Image 7',
     width: '401',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery1.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImg1,
+    src2x: GalleryImg1X2,
     alt: 'Image 1',
     width: '225',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery2.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImg2,
+    src2x: GalleryImg2X2,
     alt: 'Image 2',
     width: '225',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery3.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImg3,
+    src2x: GalleryImg3X2,
     alt: 'Image 3',
     width: '225',
     height: '286',
@@ -160,85 +202,85 @@ export const galleryImages = [
 
 export const galleryImagesMob = [
   {
-    src: '/src/assets/images/gallery1-mob.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImgMob1,
+    src2x: GalleryImgMob1X2,
     alt: 'Image 1',
     width: '329',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery2-mob.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImgMob2,
+    src2x: GalleryImgMob2X2,
     alt: 'Image 2',
     width: '323',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery3-mob.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImgMob3,
+    src2x: GalleryImgMob3X2,
     alt: 'Image 3',
     width: '317',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery4-mob.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImgMob4,
+    src2x: GalleryImgMob4X2,
     alt: 'Image 4',
     width: '305',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery1-mob.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
+    src: GalleryImgMob3,
+    src2x: GalleryImgMob3X2,
+    alt: 'Image 3',
+    width: '317',
+    height: '286',
+  },
+  {
+    src: GalleryImgMob1,
+    src2x: GalleryImgMob1X2,
     alt: 'Image 1',
     width: '329',
     height: '286',
   },
   {
-    src: '/src/assets/images/gallery2-mob.webp',
-    src2x: '/src/assets/images/gallery1.webp',
-    alt: 'Image 2',
-    width: '323',
-    height: '286',
-  },
-  {
-    src: '/src/assets/images/gallery3-mob.webp',
-    src2x: '/src/assets/images/gallery1-x2.webp',
-    alt: 'Image 3',
-    width: '317',
+    src: GalleryImgMob4,
+    src2x: GalleryImgMob4X2,
+    alt: 'Image 4',
+    width: '305',
     height: '286',
   },
 ];
 
 export const partnersImages = [
   {
-    src: '/src/assets/icons/logos_apple-app-store.svg',
+    src: PartnersIcon1,
     alt: 'Image 1',
     width: '61',
     height: '61',
   },
-  { src: '/src/assets/icons/logos_apiary.svg', alt: 'Image 2', width: '61', height: '62' },
+  { src: PartnersIcon2, alt: 'Image 2', width: '61', height: '62' },
   {
-    src: '/src/assets/icons/logos_android-icon.svg',
+    src: PartnersIcon3,
     alt: 'Image 3',
     width: '61',
     height: '72',
   },
   {
-    src: '/src/assets/icons/logos_basecamp.svg',
+    src: PartnersIcon4,
     alt: 'Image 4',
     width: '73',
     height: '61',
   },
-  { src: '/src/assets/icons/logos_airbnb.svg', alt: 'Image 4', width: '61', height: '66' },
-  { src: '/src/assets/icons/logos_ibm.svg', alt: 'Image 4', width: '151', height: '61' },
+  { src: PartnersIcon5, alt: 'Image 5', width: '61', height: '66' },
+  { src: PartnersIcon6, alt: 'Image 6', width: '151', height: '61' },
   {
-    src: '/src/assets/icons/logos_apple-app-store.svg',
-    alt: 'Image 1',
+    src: PartnersIcon7,
+    alt: 'Image 7',
     width: '61',
     height: '61',
   },
-  { src: '/src/assets/icons/logos_apiary.svg', alt: 'Image 2', width: '61', height: '62' },
+  { src: PartnersIcon2, alt: 'Image 2', width: '61', height: '62' },
 ];
 
 export const testimonialCards = [
@@ -246,25 +288,25 @@ export const testimonialCards = [
     title: 'main.testimonials.title1',
     role: 'main.testimonials.role1',
     description: 'main.testimonials.desc1',
-    src: '/src/assets/images/avatar1.png',
+    src: TestimonialImg1,
   },
   {
     title: 'main.testimonials.title2',
     role: 'main.testimonials.role2',
     description: 'main.testimonials.desc2',
-    src: '/src/assets/images/avatar2.png',
+    src: TestimonialImg2,
   },
   {
     title: 'main.testimonials.title3',
     role: 'main.testimonials.role3',
     description: 'main.testimonials.desc3',
-    src: '/src/assets/images/avatar3.png',
+    src: TestimonialImg3,
   },
   {
     title: 'main.testimonials.title4',
     role: 'main.testimonials.role4',
     description: 'main.testimonials.desc4',
-    src: '/src/assets/images/avatar4.png',
+    src: TestimonialImg4,
   },
 ];
 
